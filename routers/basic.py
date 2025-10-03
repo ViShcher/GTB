498cb9c0eb635f9d62c4804629a72b0a86db15af
Viktor <tm181tm@gmail.com>
2025-10-03 17:40:43 +0800

    Update basic.py

--- routers/basic.py
+++ routers/basic.py
@@ -1,12 +1,16 @@
 from aiogram import Router
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardRemove
 
 basic_router = Router()
 
 @basic_router.message(CommandStart())
 async def start(msg: Message):
-    await msg.answer("Привет. Живу на вебхуке v3, всё слышу. Команды: /help, /add_ex name, /list_ex")
+    # снимаем старую клавиатуру, если клиент её кэшировал
+    await msg.answer(
+        "Привет. Живу на вебхуке v3, всё слышу. Команды: /help, /add_ex name, /list_ex",
+        reply_markup=ReplyKeyboardRemove()
+    )
 
 @basic_router.message(Command("help"))
 async def help_cmd(msg: Message):
