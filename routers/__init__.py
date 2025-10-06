dc479550e9c1804bb796a16d70f130540067a443
Viktor <tm181tm@gmail.com>
2025-10-07 00:31:44 +0800

    Update __init__.py

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,6 +1,5 @@
 from .basic import basic_router
-from .workouts import workouts_router
 from .profile import profile_router
 from .training import training_router
 
-__all__ = ["basic_router", "workouts_router", "profile_router", "training_router"]
+__all__ = ["basic_router", "profile_router", "training_router"]
