bb5df5283d291c801e1bd37ee87c286c9233d115
Viktor <tm181tm@gmail.com>
2025-10-08 18:54:26 +0800

    Update __init__.py

--- routers/__init__.py
+++ routers/__init__.py
@@ -3,6 +3,14 @@ from .basic import basic_router
 from .profile import profile_router
 from .cardio import cardio_router
 from .training import training_router
+from .reports import reports_router
+__all__ = [
+    "basic_router",
+    "profile_router",
+    "training_router",
+    "cardio_router",
+    "reports_router",
+]
 
 # Порядок важен: cardio выше training, чтобы кардио-ввод не перехватывался силовым логгером
 __all__ = ["basic_router", "profile_router", "cardio_router", "training_router"]
