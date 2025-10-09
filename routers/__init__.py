d7283db1f32a7d534628005f0841391447af29e2
Viktor <tm181tm@gmail.com>
2025-10-09 18:00:53 +0800

    Update __init__.py

--- routers/__init__.py
+++ routers/__init__.py
@@ -4,10 +4,13 @@ from .profile import profile_router
 from .cardio import cardio_router
 from .training import training_router
 from .reports import reports_router
+from .feedback import feedback_router
+
 __all__ = [
     "basic_router",
     "profile_router",
     "training_router",
     "cardio_router",
     "reports_router",
+    "feedback_router",
 ]
